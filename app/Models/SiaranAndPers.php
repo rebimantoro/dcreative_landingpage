e1acef4e8b6ec8f2e476a5a0ffe64ef0d5c92911
Robby Pambudi <robby.pambudi10@gmail.com>
2022-04-11 13:00:40 +0700

    Model add event and siaran pers

---
+++ app/Models/SiaranAndPers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiaranAndPers extends Model
+{
+    use HasFactory;
+    protected $table = 'siaranandpers';
+    protected $guarded = [];
+    
+}
