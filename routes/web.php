7c0a1ebcbcc94d6ec3329b174023a00cdd4a8602
Robby Pambudi <robby.pambudi10@gmail.com>
2022-04-11 13:01:27 +0700

    Router add new

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LayananController;
 use App\Http\Controllers\FaqController;
 use App\Http\Controllers\TestimoniController;
+use App\Http\Controllers\CompetitionController;
+use App\Http\Controllers\EventController;
+use Illuminate\Support\Facades\App;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +26,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin/home', [HomeController::class, 'index'])->name('home');
 
 //Layanan
 Route::get('/admin/layanan', [LayananController::class, 'layananAdmin']);
@@ -48,4 +52,18 @@ Route::post('/admin/testimoni/{testimoni}/delete', [TestimoniController::class, 
 
 // Competition And Event
 // Competition 
-Route::get('/admin/competitionevent/competition', [App\Http\Controllers\CompetitionController::class, 'competitionAdmin']);
+Route::get('/admin/competition', [CompetitionController::class, 'competitionAdmin']);
+Route::get('/admin/competition/create', [CompetitionController::class, 'create']);
+Route::post('/admin/competition', [CompetitionController::class, 'store']);
+Route::get('/admin/competition/{competition}/edit', [CompetitionController::class, 'edit']);
+Route::post('/admin/competition/{competition}', [CompetitionController::class, 'update']);
+Route::post('/admin/competition/{competition}/delete', [CompetitionController::class, 'destroy']);
+
+
+// Event
+Route::get('/admin/event', [EventController::class, 'eventAdmin']);
+Route::get('/admin/event/create', [EventController::class, 'create']);
+Route::post('/admin/event', [EventController::class, 'store']);
+Route::get('/admin/event/{event}/edit', [EventController::class, 'edit']);
+Route::post('/admin/event/{competition}', [EventController::class, 'update']);
+Route::post('/admin/event/{event}/delete', [EventController::class, 'destroy']);
